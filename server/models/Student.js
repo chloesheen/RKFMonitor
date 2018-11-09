cef30d97726bb46792b6486e0ffdc9ee69333418
mariabelenvivanco <mvivanco@brynmawr.edu>
2018-11-09 05:57:49 +0000

    Update Student.js

--- server/models/Student.js
+++ server/models/Student.js
@@ -3,13 +3,13 @@ var mongoose = require('mongoose')
   , model = mongoose.model
 
 var studentSchema = new Schema({
-	firstName    : {type: String, required: true},
-  lastName     : {type: String, required: true},
-  // Date of Birth
-  month : {type: String, required: true},
-  day : {type: Int, required: true},
-  year : {type: Int, required: true},
-  isPresent : {type: Boolean, default: true}
+    firstName : {type: String, required: true},
+    lastName : {type: String, required: true},
+    // Date of Birth
+    month : {type: String, required: true},
+    day : {type: Int, required: true},
+    year : {type: Int, required: true},
+    isPresent : {type: Boolean, default: true}
 });
 
 module.exports = mongoose.model('Student', studentSchema);
