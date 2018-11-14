90e708355325fe618162f280aa941b6422ed39d1
mariabelenvivanco <mvivanco@brynmawr.edu>
2018-11-14 19:20:25 +0000

    Update Class.js

--- server/models/Class.js
+++ server/models/Class.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose')
   , model = mongoose.model
 
 var classSchema  = new Schema({
-	createdAt  : {type: Date, default: Date.now},
-    teacher    : {type: mongoose.Schema.Types.ObjectId, ref : 'Teacher ', required : true},
-	students   : [{type: mongoose.Schema.Types.ObjectId, ref : 'Student ', required : true}],
-    studentsPresent : {type: Int, default: 0},   //total of students present in class
+	createdAt: {type: Date, default: Date.now},
+    teacher: {type: mongoose.Schema.Types.ObjectId, ref : 'Teacher ', required : true},
+	students: [{type: mongoose.Schema.Types.ObjectId, ref : 'Student ', required : true}],
+    studentsPresent: {type: Int, default: 0},   //total of students present in class
    // only homerooms to be counted in attendance to avoid duplicates in attendance
-    isHomeroom : {type: Boolean, default: false, required : true},
+    isHomeroom: {type: Boolean, default: false, required : true},
 	school: {type: mongoose.Schema.Types.ObjectId, ref: 'School', required: true},
 });
 
