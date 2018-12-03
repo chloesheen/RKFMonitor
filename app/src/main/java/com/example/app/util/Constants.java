e7d3b3476c508567e1d1ba99dbec75063f8afc0a
Leslie Goloh <leslieg@wc137-dhcp-35.brynmawr.edu>
2018-12-03 02:53:28 -0500

    catch all asynctask

---
+++ app/src/main/java/com/example/app/util/Constants.java
@@ -0,0 +1,8 @@
+package com.example.app.util;
+
+public class Constants {
+
+    public final static String GET_REQUEST = "Get";
+    public final static String POST_REQUEST = "Post";
+    public final static String PUT_REQUEST = "Put";
+}
