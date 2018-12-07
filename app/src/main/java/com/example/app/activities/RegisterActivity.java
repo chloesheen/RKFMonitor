5233b98f22c0e68066f68f6183167bf272f21127
chloesheen <csheen@brynmawr.edu>
2018-12-07 15:09:41 -0500

    added lots of updates

--- app/src/main/java/com/example/app/activities/RegisterActivity.java
+++ app/src/main/java/com/example/app/activities/RegisterActivity.java
@@ -1,43 +1,65 @@
 package com.example.app.activities;
-import android.app.Activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.app.R;
 
-public class RegisterActivity extends Activity {
+public class RegisterActivity extends AppCompatActivity {
+    private SharedPreferences registerPrefs;
+    private EditText userName;
+    private EditText password;
+    private EditText confirmPassword;
+    private Button submitButton;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Set View to register.xml
         setContentView(R.layout.activity_register);
 
-        TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
+        userName = (EditText) findViewById(R.id.register_username_edittext);
+        password = (EditText) findViewById(R.id.register_password_edittext);
+        confirmPassword = (EditText) findViewById(R.id.confirm_password_edittext);
+        submitButton = (Button) findViewById(R.id.submit_button);
 
-        // Listening to Login Screen link
-        loginScreen.setOnClickListener(new View.OnClickListener() {
+        Intent intent = getIntent();
+        registerPrefs = getApplicationContext().getSharedPreferences(intent.getStringExtra("testKey"), MODE_PRIVATE);
 
-            public void onClick(View arg0) {
-                // Closing registration screen
-                // Switching to Login Screen/closing register screen
-                finish();
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = registerPrefs.edit();
+                if (userName.getText() != null &&
+                        password.getText() != null &&
+                        confirmPassword.getText() != null &&
+                        password.getText().toString().equals(
+                                confirmPassword.getText().toString()
+                        )) {
+                    editor.putString("user" + userName.getText().toString(), userName.getText().toString());
+                    editor.putString("password" + userName.getText().toString(), password.getText().toString());
+                    editor.commit();
+                    finish();
+                }
+
             }
+
         });
-
-
         Spinner spinner = (Spinner) findViewById(R.id.school_spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
+        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.school_array, android.R.layout.simple_spinner_item);
 // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-
-
 
     }
 }
