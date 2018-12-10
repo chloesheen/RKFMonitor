f8f255caf3ed34ea13ca409d89322d84b70e8f35
spark423 <seanpark8181@gmail.com>
2018-12-10 07:10:55 -0500

    restoring package.json

--- server/app.js
+++ server/app.js
@@ -1,33 +1,13 @@
-<<<<<<< HEAD
-const express = require('express'),
-			app = express(),
-			bodyParser = require('body-parser');
-require('dotenv').config();
-=======
 require('dotenv').config();
 const express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
 	passport = require('passport'),
 	random = require('random-name');
->>>>>>> 9772b7f578fc9943c8fd3820a08bcbdf656a6244
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-<<<<<<< HEAD
-//Mongoose==============================================================================================================================
-const	mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
-
-//Routes============================================================================================================================================================
-const	authenticate = require('./routes/authenticate');
-
-app.use('/', authenticate);
-
-//Connect===========================================================================================================================================================
-app.listen(3000, function(req, res) {
-=======
 
 //Mongoose==============================================================================================================================
 const mongoose = require('mongoose');
@@ -45,9 +25,11 @@ app.use('/organization', organization(passport));
 app.use('/teachers', teacher(passport));
 app.use('/cook', cook(passport));
 
+app.get('/', function(req, res) {
+	res.json({success: true});
+})
 
 //Connect===========================================================================================================================================================
 app.listen(process.env.PORT || 3000, function(req, res) {
->>>>>>> 9772b7f578fc9943c8fd3820a08bcbdf656a6244
 	console.log("Listening on 3000");
 })
