6171bd9040aa273af786ff19fe790ce8e1e1739c
Leslie Goloh <leslieg@Leslies-MacBook-Air.local>
2018-12-26 17:44:17 -0500

    class object update

--- app/src/main/java/com/example/app/models/Class.java
+++ app/src/main/java/com/example/app/models/Class.java
@@ -1,5 +1,7 @@
 package com.example.app.models;
 
+import java.util.ArrayList;
+
 public class Class {
 
     private String mClassName;
@@ -10,12 +12,14 @@ public class Class {
 
     private String mHomeroomTeacher;
 
+    private ArrayList<Calendar> mClasscalendar;
 
     public Class(String name, String id, String size, String teacher) {
         mClassName = name;
         mId = id;
         mClassSize = size;
         mHomeroomTeacher = teacher;
+        mClasscalendar = new ArrayList<>();
     }
 
     public String getName() {
@@ -31,4 +35,12 @@ public class Class {
     }
 
     public String getTeacher() { return mHomeroomTeacher; }
+
+    public ArrayList<Calendar> getCalendars() {
+        return mClasscalendar;
+    }
+
+    public void addCalendar(Calendar calendar) {
+        mClasscalendar.add(calendar);
+    }
 }
